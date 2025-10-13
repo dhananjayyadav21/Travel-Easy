61bec60e3d59ec3e64eb1bf93143b4bcfa433184
Sanjay Yadav <dhananjayyadav1807@gmail.com>
2025-10-14 04:09:50 +0530

    update homepage

--- app/Homepage/page.jsx
+++ app/Homepage/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useState, useEffect } from 'react';
 import TopTravelProviders from '@/components/TopTravelProviders';
 import TravelInfo from '@/components/TravelInfo';
