dd7c7691002f47387b2f69859bd0c4ee64ab962e
Sanjay Yadav <dhananjayyadav1807@gmail.com>
2025-10-14 14:15:35 +0530

    fix body color

--- app/layout.js
+++ app/layout.js
@@ -22,10 +22,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-red-600`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gradient-to-r from-green-50 to-red-50`}
       >
         <Navbar />
-        <main className="min-h-screen pt-14 bg-amber-600">
+        <main className="min-h-screen pt-14 bg-gradient-to-r from-green-50 to-red-50">
           {children}
           <Toaster
             position="bottom-right"
